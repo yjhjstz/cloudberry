greenplum-db/gpdb-archive

pg_resgroup_move_query() improvements. (#15411)
The pg_resgroup_move_query() function implemented in 51ee26b has
several disadvantages:
1) Slot leaking. The slot acquired in initiator process doesn't free if
target process isn't alive, didn't receive a signal, or even received,
but at the time it was in idle state.
2) Race condition between UnassignResGroup() called at the end of
transaction (or honestly any other code) and handler called immediately
after USR1 signal.
3) Not working entrydb process moving. Previously, the USR1 signal was
sent to only one process, target or entrydb. Entrydb moving was never
tested properly.

Improvements added to solve the first problem:
1) Feedback from target process to initiator. Target process can set
initiator's latch to quickly interrupt pg_resgroup_move_query() from
awaiting.
2) Existed movetoResSlot parameter acts like a mark. Initiator sets it
and waits on latch. If movetoResSlot become NULL, slot control is on
the target process side.
3) Initiator PID. Used by target process to get initiator's latch.
4) Mutex. To guard all critical moveto* parameters from parallel
changes.

To solve the second problem, there was an attempt to use Postgres
interruptions. I was unhappy to know GPDB use raw InterruptPending
value to do some pre-cancellation of dispatched queries. GPDB thinks
InterruptPending can be triggered only by "negative" events, which
leads to cancelation. The temporary solution with InterruptPending-like
"positive" flag showed that we may wait for next CHECK_FOR_INTERRUPTS()
call for a long. For example, some fat insert may do group moving only
at the end of dispatching, which makes no sense.
Thus, I decided to decrease the probability of races by using
additional IsTransactionState() check.
IMPORTANT NOTICE. The existed solution still affected by race
conditions. The current handler's implementation is an example of bad
design and should be reworked.

To solve the third problem, no

concourse/scripts/ic_gpdb_resgroup.bash
@@ -52,6 +52,14 @@ make_cgroups_dir() {
 EOF
 }
 
+install_python_dependency() {
+    local host_alias=$1
+
+    ssh $host_alias bash -ex <<EOF
+        pip3.9 install paramiko==3.1.0
+EOF
+}
+
 run_resgroup_test() {
     local gpdb_master_alias=$1
 
@@ -142,6 +150,9 @@ mount_cgroups ccp-${CLUSTER_NAME}-0
 mount_cgroups ccp-${CLUSTER_NAME}-1
 make_cgroups_dir ccp-${CLUSTER_NAME}-0
 make_cgroups_dir ccp-${CLUSTER_NAME}-1
+install_python_dependency ccp-${CLUSTER_NAME}-0
+install_python_dependency ccp-${CLUSTER_NAME}-1
+install_python_dependency cdw
 run_resgroup_test cdw
 
 #

yjhjstz/cloudberry

concourse/scripts/ic_gpdb_resgroup.bash
@@ -52,6 +52,14 @@ make_cgroups_dir() {
 EOF
 }
 
+install_python_dependency() {
+    local host_alias=$1
+
+    ssh $host_alias bash -ex <<EOF
+        pip3.9 install paramiko==3.1.0
+EOF
+}
+
 run_resgroup_test() {
     local gpdb_master_alias=$1
 
@@ -142,6 +150,9 @@ mount_cgroups ccp-${CLUSTER_NAME}-0
 mount_cgroups ccp-${CLUSTER_NAME}-1
 make_cgroups_dir ccp-${CLUSTER_NAME}-0
 make_cgroups_dir ccp-${CLUSTER_NAME}-1
+install_python_dependency ccp-${CLUSTER_NAME}-0
+install_python_dependency ccp-${CLUSTER_NAME}-1
+install_python_dependency mdw
 run_resgroup_test mdw
 
 #