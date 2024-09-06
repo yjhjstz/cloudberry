c4748dee80e334537ad9dda8f81e586f41a824d9
liuxiaoyu <liuxiaoyu@hashdata.cn>
2024-09-06 14:18:26 +0800

    Fix datalake ci

--- contrib/datalake_fdw/test/datalake_test.bash
+++ contrib/datalake_fdw/test/datalake_test.bash
@@ -1,7 +1,7 @@
 #set -euo pipefail
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source $BASE_DIR/create_env.sh
-source $BASE_DIR/function.sh
+source $BASE_DIR/functions.sh
 
 
 function start_test() {
