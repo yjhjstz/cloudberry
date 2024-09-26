fanfuxiaoran/cloudberrydb

Add force param to ExecSquelchNode
Each node squelch function, such as 'ExecSquelchFunctionScan', it only
cares if its self node has been squelched, if not then do that.

No matter if the node is squelched or not, each node's squelch function
will call ExecSquelchNode for its child nodes.

For some special nodes, such as MaterialNode or SortNode, which have
'delayEagerFree' field and 'delayEagerFree' is true,  only can be squelched
when force is true.

src/backend/executor/nodeMemoize.c
@@ -1127,9 +1127,10 @@ ExecReScanMemoize(MemoizeState *node)
 }
 
 void
-ExecSquelchMemoize(MemoizeState *node)
+ExecSquelchMemoize(MemoizeState *node, bool force)
 {
-	ExecSquelchNode(outerPlanState(node));
+	node->ss.pq.squelched = true;
+	ExecSquelchNode(outerPlanState(node), force);
 }
 
 /*

yjhjstz/cloudberry

(cherry picked from commit f9f89c3d0719fa1250935d60b810c6673337d2d6)

src/backend/executor/nodeMemoize.c
@@ -1127,9 +1127,10 @@ ExecReScanMemoize(MemoizeState *node)
 }
 
 void
-ExecSquelchMemoize(MemoizeState *node)
+ExecSquelchMemoize(MemoizeState *node, bool force)
 {
-	ExecSquelchNode(outerPlanState(node));
+	node->ss.ps.squelched = true;
+	ExecSquelchNode(outerPlanState(node), force);
 }
 
 /*