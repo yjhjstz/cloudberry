163a56e90f7251ff7016b6727e4a81fe12bb8da6
Dianjin Wang <wangdianjin@gmail.com>
2025-03-11 11:48:02 +0800

    Epic: Rebrand Names and URLs for ASF Compliance (#731)
    This commit consolidates changes to align the project with ASF brand
    standards, improve clarity, and ensure consistent naming and
    references throughout the codebase.
    
    Key Changes:
    
    1. Rebranding
    
    * Renamed all instances of `Cloudberry Database`, `CloudberryDB`, and
      similar terms to `Apache Cloudberry` or `Cloudberry` as appropriate.
    * Applied consistent naming across:
      - `contrib/*`
      - `gpAux/*`
      - `gpMgmt/*`
      - `gpcontrib/*`
      - `src/*` (e.g., src/backend, src/bin, src/include, src/test, etc.)
      - `doc/*`
    * Replace Greenplum with Apache Cloudberry in the user-facing places.
    
    2. URL Updates
    
    * Updated outdated GitHub repository links to the new Apache project
      location.
    
    These changes are part of an ongoing effort to ensure ASF compliance.
    
    3. Fix the tests to pass the checks.
    
    (cherry picked from commit b64d461e37cc7e8f1dd7beffa95b490b54d2c5ab)

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -501,7 +501,7 @@ SocketBackend(StringInfo inBuf)
 			doing_extended_query_message = false;
 			break;
 
-		case 'M':				/* Cloudberry Database dispatched statement from QD */
+		case 'M':				/* Apache Cloudberry dispatched statement from QD */
 			maxmsglen = PQ_LARGE_MESSAGE_LIMIT;
 			doing_extended_query_message = false;
 
@@ -514,7 +514,7 @@ SocketBackend(StringInfo inBuf)
 
 			break;
 
-		case 'T':				/* Cloudberry Database dispatched transaction protocol from QD */
+		case 'T':				/* Apache Cloudberry dispatched transaction protocol from QD */
 			maxmsglen = PQ_LARGE_MESSAGE_LIMIT;
 			doing_extended_query_message = false;
 
