greenplum-db/gpdb

Optimize DELETEs in Orca to only project necessary columns
Currently, ORCA projects all columns for DML operations, which means
that deleting a row requires populating all of the columns in the
targetlist. For deletes, the only necessary columns are the ctid and
gp_segment_id in the executor. This performance difference is especially
evident for CO tables

Things are more complicated due to how Orca handles partitioned tables.
Orca produces DynamicScan nodes, which scan the necessary partitions and
pass those tuples to the DML operator. While planner uses separate scan
and each partition corresponds to a delete operator, for Orca the DML
operator needs to figure out which partition to delete from for each
tuple. It does this through tuple routing logic in the executor. Because
of this, Orca needs to project the partition key and include additional
mapping logic to remap this partition key to fit the existing tuple
routing framework. Additionally, internally Orca needs the distribution
key to figure out which segment to use for direct dispatch.

For intermediate/mid-level partitions, we don't have the partition keys
of the top level/parent partition exposed in Orca. However, when
performing the tuple routing logic in the executor, it is expected that
all partition keys exist in the project list.

For this performance optimization, we'll limit it to deleting from leaf-level or
top-level partitions. While we'll still support deleting from mid-level
partitions, all of the columns will be projected.

Performance numbers:

Using the setup from https://github.com/greenplum-db/gpdb/issues/16755.

```
CREATE TABLE wide_table(i int, j int, k int, l int, m int, n int, o int)
WITH(appendonly=true,orientation=column,compresstype=zlib,compresslevel=5);
INSERT INTO wide_table SELECT g, g+1, g+2, g+3, g+4, g+5, g+6 FROM
generate_series(1, 100000000) g; begin; explain (analyze, verbose)
delete from wide_table; rollback;
```

Before: 10.2s
After: 6.4s

src/backend/access/common/tupconvert.c
@@ -329,6 +329,86 @@ convert_tuples_by_name_map(TupleDesc indesc,
 }
 
 /*
+ * Return a palloc'd bare attribute map for tuple conversion, matching input
+ * and output columns by name.  (Dropped columns are ignored in both input and
+ * output.)  This is normally a subroutine for convert_tuples_by_name, but can
+ * be used standalone.
+ *
+ * NOTE: this is identical to the above function, but does
+ * not throw an error if an attribute in the outdesc is missing from the indesc.
+ * Future postgres merges will add missing_ok as a parameter to the function and
+ * this can then be removed (commits e1551f9 and ad86d15)
+ */
+AttrNumber *
+convert_tuples_by_name_map_missing_ok(TupleDesc indesc,
+						   TupleDesc outdesc)
+{
+	AttrNumber *attrMap;
+	int			outnatts;
+	int			innatts;
+	int			i;
+	int			nextindesc = -1;
+
+	outnatts = outdesc->natts;
+	innatts = indesc->natts;
+
+	attrMap = (AttrNumber *) palloc0(outnatts * sizeof(AttrNumber));
+	for (i = 0; i < outnatts; i++)
+	{
+		Form_pg_attribute outatt = TupleDescAttr(outdesc, i);
+		char	   *attname;
+		Oid			atttypid;
+		int32		atttypmod;
+		int			j;
+
+		if (outatt->attisdropped)
+			continue;			/* attrMap[i] is already 0 */
+		attname = NameStr(outatt->attname);
+		atttypid = outatt->atttypid;
+		atttypmod = outatt->atttypmod;
+
+		/*
+		 * Now search for an attribute with the same name in the indesc. It
+		 * seems likely that a partitioned table will have the attributes in
+		 * the same order as the partition, so the search below is optimized
+		 * for that case.  It is possible that columns are dropped in one of
+		 * the relations, but not the other, so we use the 'nextindesc'
+		 * counter to track the starting point of the search.  If the inner
+		 * loop encounters dropped columns then it will have to skip over
+		 * them, but it should leave 'nextindesc' at the correct position for
+		 * the next outer loop.
+		 */
+		for (j = 0; j < innatts; j++)
+		{
+			Form_pg_attribute inatt;
+
+			nextindesc++;
+			if (nextindesc >= innatts)
+				nextindesc = 0;
+
+			inatt = TupleDescAttr(indesc, nextindesc);
+			if (inatt->attisdropped)
+				continue;
+			if (strcmp(attname, NameStr(inatt->attname)) == 0)
+			{
+				/* Found it, check type */
+				if (atttypid != inatt->atttypid || atttypmod != inatt->atttypmod)
+					ereport(ERROR,
+							(errcode(ERRCODE_DATATYPE_MISMATCH),
+							 errmsg("could not convert row type"),
+							 errdetail("Attribute \"%s\" of type %s does not match corresponding attribute of type %s.",
+									   attname,
+									   format_type_be(outdesc->tdtypeid),
+									   format_type_be(indesc->tdtypeid))));
+				attrMap[i] = inatt->attnum;
+				break;
+			}
+		}
+	}
+	return attrMap;
+}
+
+/*
  * Returns mapping created by convert_tuples_by_name_map, or NULL if no
  * conversion not required. This is a convenience routine for
  * convert_tuples_by_name() and other functions.

src/backend/executor/execPartition.c
@@ -37,6 +37,13 @@
 #include "cdb/cdbaocsam.h"
 #include "cdb/cdbappendonlyam.h"
 
+/*
+ * Helper macro that is used to determine if a Modifytable node came from a
+ * Dynamic scan (produced by Orca), which requires tuple routing to determine
+ * the correct partition
+ */
+#define IsDynamicScan(plan) castNode(ModifyTable, plan) != NULL && \
+							castNode(ModifyTable, plan)->forceTupleRouting
 /*-----------------------
  * PartitionTupleRouting - Encapsulates all information required to
  * route a tuple inserted into a partitioned table to one of its leaf
@@ -184,7 +191,8 @@ static void FormPartitionKeyDatum(PartitionDispatch pd,
 								  TupleTableSlot *slot,
 								  EState *estate,
 								  Datum *values,
-								  bool *isnull);
+								  bool *isnull,
+								  AttrNumber *attno_map);
 
 static char *ExecBuildSlotPartitionKeyDescription(Relation rel,
 												  Datum *values,
@@ -324,7 +332,23 @@ ExecFindPartition(ModifyTableState *mtstate,
 		 * So update ecxt_scantuple accordingly.
 		 */
 		ecxt->ecxt_scantuple = slot;
-		FormPartitionKeyDatum(dispatch, slot, estate, values, isnull);
+		/*
+		 * If the operation is delete and its child is a dynamic scan,
+		 * then we need to remap the the attributes from the tuple to the relation.
+		 * This is because the tuple descriptor's columns do not exactly match
+		 * the relation attributes in the catalog--the tuple has a subset of the attrs.
+		 * FormPartitionKeyDatum uses this map to get the partition key index in
+		 * the tuple descriptor, and the values at that index within the tuple.
+		 */
+		AttrNumber *attno_map = NULL;
+		if (IsDynamicScan(mtstate->ps.plan)
+			&& mtstate->operation == CMD_DELETE)
+		{
+			attno_map = convert_tuples_by_name_map_missing_ok(slot->tts_tupleDescriptor,
+															  RelationGetDescr(dispatch->reldesc));
+		}
+		/* Populate values/isnull with partition key value from tuple */
+		FormPartitionKeyDatum(dispatch, slot, estate, values, isnull, attno_map);
 
 		/*
 		 * If this partitioned table has no partitions or no partition for
@@ -496,9 +520,32 @@ ExecFindPartition(ModifyTableState *mtstate,
 				else
 					slot = rootslot;
 			}
+			/*
+			 * If this is a DELETE operation with a dynamic scan child,
+			 * then we need to convert the slot to be based on the relation descriptor, as
+			 * ExecPartitionCheck uses the partition bounds defined on the relation, not the slot.
+			 * The tuple slot returned by the scan however only includes only the projection columns.
+			 */
+			if (IsDynamicScan(mtstate->ps.plan)
+				&& mtstate->operation == CMD_DELETE)
+			{
+				TupleTableSlot *slotOut = MakeSingleTupleTableSlot(RelationGetDescr(rri->ri_RelationDesc), &TTSOpsVirtual);
+				slot = execute_attr_map_slot(attno_map, slot, slotOut);
+				ExecPartitionCheck(rri, slot, estate, true);
 
-			ExecPartitionCheck(rri, slot, estate, true);
+				/*
+				 * Drop the slot. Note that this slot points to slotOut and myslot,
+				 * so we drop it to properly clean up the slot and relation descriptor
+				 */
+				ExecDropSingleTupleTableSlot(slot);
+			}
+			else
+			{
+				ExecPartitionCheck(rri, slot, estate, true);
+			}
 		}
+		if (attno_map)
+			pfree(attno_map);
 	}
 
 	/* Release the tuple in the lowest parent's dedicated slot. */
@@ -1274,6 +1321,8 @@ ExecCleanupTupleRouting(ModifyTableState *mtstate,
  *					expressions (must be non-NULL)
  *	values			Array of partition key Datums (output area)
  *	isnull			Array of is-null indicators (output area)
+ *	attno_map		Map of att nums from tuple descriptor to relation, needed since
+ *					partition bound attnums correspond to relation, not tuple desc (GPDB only)
  *
  * the ecxt_scantuple slot of estate's per-tuple expr context must point to
  * the heap tuple passed in.
@@ -1284,7 +1333,8 @@ FormPartitionKeyDatum(PartitionDispatch pd,
 					  TupleTableSlot *slot,
 					  EState *estate,
 					  Datum *values,
-					  bool *isnull)
+					  bool *isnull,
+					  AttrNumber *attno_map)
 {
 	ListCell   *partexpr_item;
 	int			i;
@@ -1303,6 +1353,10 @@ FormPartitionKeyDatum(PartitionDispatch pd,
 	for (i = 0; i < pd->key->partnatts; i++)
 	{
 		AttrNumber	keycol = pd->key->partattrs[i];
+		/* Use passed in map to extract part key, as slot's attrs may not match the Relation's attrs */
+		if (attno_map)
+			keycol = attno_map[pd->key->partattrs[i]-1];
+
 		Datum		datum;
 		bool		isNull;
 

src/include/access/tupconvert.h
@@ -43,6 +43,8 @@ extern TupleConversionMap *convert_tuples_by_name(TupleDesc indesc,
 extern AttrNumber *convert_tuples_by_name_map(TupleDesc indesc,
 											  TupleDesc outdesc,
 											  const char *msg);
+extern AttrNumber *convert_tuples_by_name_map_missing_ok(TupleDesc indesc,
+											  TupleDesc outdesc);
 extern AttrNumber *convert_tuples_by_name_map_if_req(TupleDesc indesc,
 													 TupleDesc outdesc,
 													 const char *msg);

yjhjstz/cloudberry

(cherry picked from commit 7e31f8404ce2a0059f5518be3498888a6dfc774f)

src/backend/access/common/tupconvert.c
@@ -20,7 +20,7 @@
 
 #include "access/tupconvert.h"
 #include "executor/tuptable.h"
-
+#include "utils/builtins.h"
 
 /*
  * The conversion setup routines have the following common API:
@@ -130,6 +130,87 @@ convert_tuples_by_name(TupleDesc indesc,
 	map->inisnull[0] = true;
 
 	return map;
+}
+
+/*
+ * Return a palloc'd bare attribute map for tuple conversion, matching input
+ * and output columns by name.  (Dropped columns are ignored in both input and
+ * output.)  This is normally a subroutine for convert_tuples_by_name, but can
+ * be used standalone.
+ *
+ * NOTE: this is identical to the above function, but does
+ * not throw an error if an attribute in the outdesc is missing from the indesc.
+ * Future postgres merges will add missing_ok as a parameter to the function and
+ * this can then be removed (commits e1551f9 and ad86d15)
+ */
+AttrMap *
+convert_tuples_by_name_map_missing_ok(TupleDesc indesc,
+						   TupleDesc outdesc)
+{
+	AttrMap *attrMap;
+	int			outnatts;
+	int			innatts;
+	int			i;
+	int			nextindesc = -1;
+
+	outnatts = outdesc->natts;
+	innatts = indesc->natts;
+
+	attrMap = make_attrmap(outnatts);
+
+	for (i = 0; i < outnatts; i++)
+	{
+		Form_pg_attribute outatt = TupleDescAttr(outdesc, i);
+		char	   *attname;
+		Oid			atttypid;
+		int32		atttypmod;
+		int			j;
+
+		if (outatt->attisdropped)
+			continue;			/* attrMap[i] is already 0 */
+		attname = NameStr(outatt->attname);
+		atttypid = outatt->atttypid;
+		atttypmod = outatt->atttypmod;
+
+		/*
+		 * Now search for an attribute with the same name in the indesc. It
+		 * seems likely that a partitioned table will have the attributes in
+		 * the same order as the partition, so the search below is optimized
+		 * for that case.  It is possible that columns are dropped in one of
+		 * the relations, but not the other, so we use the 'nextindesc'
+		 * counter to track the starting point of the search.  If the inner
+		 * loop encounters dropped columns then it will have to skip over
+		 * them, but it should leave 'nextindesc' at the correct position for
+		 * the next outer loop.
+		 */
+		for (j = 0; j < innatts; j++)
+		{
+			Form_pg_attribute inatt;
+
+			nextindesc++;
+			if (nextindesc >= innatts)
+				nextindesc = 0;
+
+			inatt = TupleDescAttr(indesc, nextindesc);
+			if (inatt->attisdropped)
+				continue;
+			if (strcmp(attname, NameStr(inatt->attname)) == 0)
+			{
+				/* Found it, check type */
+				if (atttypid != inatt->atttypid || atttypmod != inatt->atttypmod)
+					ereport(ERROR,
+							(errcode(ERRCODE_DATATYPE_MISMATCH),
+							 errmsg("could not convert row type"),
+							 errdetail("Attribute \"%s\" of type %s does not match corresponding attribute of type %s.",
+									   attname,
+									   format_type_be(outdesc->tdtypeid),
+									   format_type_be(indesc->tdtypeid))));
+				attrMap->attnums[i] = inatt->attnum;
+				break;
+			}
+		}
+	}
+	return attrMap;
 }
 
 /*

src/backend/executor/execPartition.c
@@ -37,6 +37,13 @@
 #include "cdb/cdbaocsam.h"
 #include "cdb/cdbappendonlyam.h"
 
+/*
+ * Helper macro that is used to determine if a Modifytable node came from a
+ * Dynamic scan (produced by Orca), which requires tuple routing to determine
+ * the correct partition
+ */
+#define IsDynamicScan(plan) castNode(ModifyTable, plan) != NULL && \
+							castNode(ModifyTable, plan)->forceTupleRouting
 /*-----------------------
  * PartitionTupleRouting - Encapsulates all information required to
  * route a tuple inserted into a partitioned table to one of its leaf
@@ -176,7 +183,8 @@ static void FormPartitionKeyDatum(PartitionDispatch pd,
 								  TupleTableSlot *slot,
 								  EState *estate,
 								  Datum *values,
-								  bool *isnull);
+								  bool *isnull,
+								  AttrNumber *attno_map);
 
 static char *ExecBuildSlotPartitionKeyDescription(Relation rel,
 												  Datum *values,
@@ -304,7 +312,23 @@ ExecFindPartition(ModifyTableState *mtstate,
 		 * So update ecxt_scantuple accordingly.
 		 */
 		ecxt->ecxt_scantuple = slot;
-		FormPartitionKeyDatum(dispatch, slot, estate, values, isnull);
+		/*
+		 * If the operation is delete and its child is a dynamic scan,
+		 * then we need to remap the the attributes from the tuple to the relation.
+		 * This is because the tuple descriptor's columns do not exactly match
+		 * the relation attributes in the catalog--the tuple has a subset of the attrs.
+		 * FormPartitionKeyDatum uses this map to get the partition key index in
+		 * the tuple descriptor, and the values at that index within the tuple.
+		 */
+		AttrMap *attno_map = NULL;
+		if (IsDynamicScan(mtstate->ps.plan)
+			&& mtstate->operation == CMD_DELETE)
+		{
+			attno_map = convert_tuples_by_name_map_missing_ok(slot->tts_tupleDescriptor,
+															  RelationGetDescr(dispatch->reldesc));
+		}
+		/* Populate values/isnull with partition key value from tuple */
+		FormPartitionKeyDatum(dispatch, slot, estate, values, isnull, attno_map->attnums);
 
 		/*
 		 * If this partitioned table has no partitions or no partition for
@@ -470,9 +494,32 @@ ExecFindPartition(ModifyTableState *mtstate,
 				else
 					slot = rootslot;
 			}
+			/*
+			 * If this is a DELETE operation with a dynamic scan child,
+			 * then we need to convert the slot to be based on the relation descriptor, as
+			 * ExecPartitionCheck uses the partition bounds defined on the relation, not the slot.
+			 * The tuple slot returned by the scan however only includes only the projection columns.
+			 */
+			if (IsDynamicScan(mtstate->ps.plan)
+				&& mtstate->operation == CMD_DELETE)
+			{
+				TupleTableSlot *slotOut = MakeSingleTupleTableSlot(RelationGetDescr(rri->ri_RelationDesc), &TTSOpsVirtual);
+				slot = execute_attr_map_slot(attno_map, slot, slotOut);
+				ExecPartitionCheck(rri, slot, estate, true);
 
-			ExecPartitionCheck(rri, slot, estate, true);
+				/*
+				 * Drop the slot. Note that this slot points to slotOut and myslot,
+				 * so we drop it to properly clean up the slot and relation descriptor
+				 */
+				ExecDropSingleTupleTableSlot(slot);
+			}
+			else
+			{
+				ExecPartitionCheck(rri, slot, estate, true);
+			}
 		}
+		if (attno_map)
+			free_attrmap(attno_map);
 	}
 
 	/* Release the tuple in the lowest parent's dedicated slot. */
@@ -1210,6 +1257,8 @@ ExecCleanupTupleRouting(ModifyTableState *mtstate,
  *					expressions (must be non-NULL)
  *	values			Array of partition key Datums (output area)
  *	isnull			Array of is-null indicators (output area)
+ *	attno_map		Map of att nums from tuple descriptor to relation, needed since
+ *					partition bound attnums correspond to relation, not tuple desc (GPDB only)
  *
  * the ecxt_scantuple slot of estate's per-tuple expr context must point to
  * the heap tuple passed in.
@@ -1220,7 +1269,8 @@ FormPartitionKeyDatum(PartitionDispatch pd,
 					  TupleTableSlot *slot,
 					  EState *estate,
 					  Datum *values,
-					  bool *isnull)
+					  bool *isnull,
+					  AttrNumber *attno_map)
 {
 	ListCell   *partexpr_item;
 	int			i;
@@ -1239,6 +1289,10 @@ FormPartitionKeyDatum(PartitionDispatch pd,
 	for (i = 0; i < pd->key->partnatts; i++)
 	{
 		AttrNumber	keycol = pd->key->partattrs[i];
+		/* Use passed in map to extract part key, as slot's attrs may not match the Relation's attrs */
+		if (attno_map)
+			keycol = attno_map[pd->key->partattrs[i]-1];
+
 		Datum		datum;
 		bool		isNull;
 

src/include/access/tupconvert.h
@@ -39,7 +39,8 @@ extern TupleConversionMap *convert_tuples_by_position(TupleDesc indesc,
 
 extern TupleConversionMap *convert_tuples_by_name(TupleDesc indesc,
 												  TupleDesc outdesc);
-
+extern AttrMap *convert_tuples_by_name_map_missing_ok(TupleDesc indesc,
+											  TupleDesc outdesc);
 extern HeapTuple execute_attr_map_tuple(HeapTuple tuple, TupleConversionMap *map);
 extern TupleTableSlot *execute_attr_map_slot(AttrMap *attrMap,
 											 TupleTableSlot *in_slot,