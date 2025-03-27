greenplum-db/gpdb-archive

Add warning and guard code for zero-column table.
This commit does two things:
  1. print a warning log when user try to create
     a table with no columns
  2. add guard code that distributed zero-column
     table cannot have distributed keys.

src/backend/commands/tablecmds.c
@@ -931,6 +931,20 @@ DefineRelation(CreateStmt *stmt, char relkind, Oid ownerId,
 	else
 		policy = getPolicyForDistributedBy(stmt->distributedBy, descriptor);
 
+	/* Greenplum specific code */
+	if (list_length(schema) == 0)
+	{
+		elogif(Gp_role == GP_ROLE_DISPATCH, WARNING,
+			   "creating a table with no columns.");
+
+		/*
+		 * Guard code: Github Issue 17271. Zero-column table
+		 * if distributed, must have randomly policy.
+		 */
+		if (GpPolicyIsHashPartitioned(policy))
+			policy = createRandomPartitionedPolicy(policy->numsegments);
+	}
+
 	if (partitioned && GpPolicyIsReplicated(policy))
 		ereport(ERROR,
 				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),

yjhjstz/cloudberry

fix misc_jiras
(cherry picked from commit 7c9a7824ae9c9d8d8b90205d4c84afee8055777b)

src/backend/commands/tablecmds.c
@@ -1037,6 +1037,20 @@ DefineRelation(CreateStmt *stmt, char relkind, Oid ownerId,
 	else
 		policy = getPolicyForDistributedBy(stmt->distributedBy, descriptor);
 
+	/* Greenplum specific code */
+	if (list_length(schema) == 0)
+	{
+		elogif(Gp_role == GP_ROLE_DISPATCH, LOG,
+			   "creating a table with no columns.");
+
+		/*
+		 * Guard code: Github Issue 17271. Zero-column table
+		 * if distributed, must have randomly policy.
+		 */
+		if (GpPolicyIsHashPartitioned(policy))
+			policy = createRandomPartitionedPolicy(policy->numsegments);
+	}
+
 	if (partitioned && GpPolicyIsReplicated(policy))
 		ereport(ERROR,
 				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),