a3a51621eda4b9fe943498e1f880c6c33b3f88f4
zhangyue <zhangyue@hashdata.cn>
2025-07-02 16:22:29 +0800

    Add runtime filter pushdown support for DynamicSeqscan operator
    - Extend runtime filter pushdown to DynamicSeqscan;
    - Update related executor nodes and headers;
    - Update regression tests

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -139,8 +139,7 @@ static void show_incremental_sort_info(IncrementalSortState *incrsortstate,
 static void show_hash_info(HashState *hashstate, ExplainState *es);
 static void show_runtime_filter_info(RuntimeFilterState *rfstate,
 									 ExplainState *es);
-static void show_pushdown_runtime_filter_info(const char *qlabel,
-											  PlanState *planstate,
+static void show_pushdown_runtime_filter_info(PlanState *planstate,
 											  ExplainState *es);
 static void show_memoize_info(MemoizeState *mstate, List *ancestors,
 							  ExplainState *es);
@@ -2591,8 +2590,7 @@ ExplainNode(PlanState *planstate, List *ancestors,
 			/* FALLTHROUGH */
 		case T_SeqScan:
 			if (gp_enable_runtime_filter_pushdown && IsA(planstate, SeqScanState))
-				show_pushdown_runtime_filter_info("Rows Removed by Pushdown Runtime Filter",
-												  planstate, es);
+				show_pushdown_runtime_filter_info(planstate, es);
 			/* FALLTHROUGH */
 		case T_DynamicSeqScan:
 		case T_ValuesScan:
@@ -2601,6 +2599,9 @@ ExplainNode(PlanState *planstate, List *ancestors,
 		case T_WorkTableScan:
 		case T_SubqueryScan:
 			if (IsA(plan, DynamicSeqScan)) {
+				if (gp_enable_runtime_filter_pushdown)
+					show_pushdown_runtime_filter_info(planstate, es);
+
 				char *buf;
 				Oid relid;
 				relid = rt_fetch(((DynamicSeqScan *)plan)
@@ -4509,17 +4510,19 @@ show_instrumentation_count(const char *qlabel, int which,
  * runtime filter.
  */
 static void
-show_pushdown_runtime_filter_info(const char *qlabel,
-								  PlanState *planstate,
-								  ExplainState *es)
+show_pushdown_runtime_filter_info(PlanState *planstate, ExplainState *es)
 {
-	Assert(gp_enable_runtime_filter_pushdown && IsA(planstate, SeqScanState));
+	Assert(gp_enable_runtime_filter_pushdown && 
+		   (IsA(planstate, SeqScanState) || IsA(planstate, DynamicSeqScanState)));
 
 	if (!es->analyze || !planstate->instrument)
 		return;
 
 	if (planstate->instrument->prf_work)
-		ExplainPropertyFloat(qlabel, NULL, planstate->instrument->nfilteredPRF, 0, es);
+	{
+		ExplainPropertyFloat("Rows Removed by Pushdown Runtime Filter",
+							 NULL, planstate->instrument->nfilteredPRF, 0, es);
+	}
 }
 
 /*
