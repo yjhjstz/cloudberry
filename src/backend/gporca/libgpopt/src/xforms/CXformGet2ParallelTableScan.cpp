6dd49aec2dc2a1a03d21c55980fffc058415d319
Jianghua Yang <yjhjstz@gmail.com>
2025-09-22 15:05:45 +0000

    fix bug correlated subquery

--- src/backend/gporca/libgpopt/src/xforms/CXformGet2ParallelTableScan.cpp
+++ src/backend/gporca/libgpopt/src/xforms/CXformGet2ParallelTableScan.cpp
@@ -62,6 +62,13 @@ CXformGet2ParallelTableScan::CXformGet2ParallelTableScan(CMemoryPool *mp)
 CXform::EXformPromise
 CXformGet2ParallelTableScan::Exfp(CExpressionHandle &exprhdl) const
 {
+	// Check if there are outer references (correlated subquery)
+	if (exprhdl.HasOuterRefs())
+	{
+		//FIXME: consider parallel scan for correlated subqueries
+		return CXform::ExfpNone;  // Don't use parallel scan with outer references
+	}
+
 	// Check if parallel plans are enabled in context and parallel processing is safe
 	if (!gpdb::IsParallelModeOK())
 	{
