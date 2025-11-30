3e7962fceb385dc105df2b9546abc70db1e4b577
Jianghua Yang <yjhjstz@gmail.com>
2025-12-01 01:22:41 +0800

    fix DXL-to-PlStmt Translation: Attribute number 23 not found in project list
    fix result node

--- src/backend/gporca/libgpopt/src/operators/CPhysicalComputeScalar.cpp
+++ src/backend/gporca/libgpopt/src/operators/CPhysicalComputeScalar.cpp
@@ -19,6 +19,7 @@
 #include "gpopt/base/CDistributionSpecReplicated.h"
 #include "gpopt/base/CDistributionSpecRouted.h"
 #include "gpopt/base/CDistributionSpecStrictSingleton.h"
+#include "gpopt/base/CDistributionSpecWorkerRandom.h"
 #include "gpopt/base/COptCtxt.h"
 #include "gpopt/operators/CExpressionHandle.h"
 
@@ -191,7 +192,7 @@ CPhysicalComputeScalar::PdsRequired(CMemoryPool *mp, CExpressionHandle &exprhdl,
 
 	// if required distribution uses any defined column, it has to be enforced on top of ComputeScalar,
 	// in this case, we request Any distribution from the child
-	if (CDistributionSpec::EdtHashed == edtRequired)
+	if (CDistributionSpec::EdtHashed == edtRequired) //FIXME
 	{
 		CDistributionSpecHashed *pdshashed =
 			CDistributionSpecHashed::PdsConvert(pdsRequired);
@@ -215,6 +216,29 @@ CPhysicalComputeScalar::PdsRequired(CMemoryPool *mp, CExpressionHandle &exprhdl,
 		if (fUsesDefinedCols)
 		{
 			return GPOS_NEW(mp) CDistributionSpecAny(this->Eopid());
+		}
+	}
+
+	if (CDistributionSpec::EdtWorkerRandom == edtRequired)
+	{
+		CDistributionSpecWorkerRandom *pdsWorkerRandom =
+			CDistributionSpecWorkerRandom::PdsConvert(pdsRequired);
+
+		// if base distribution is hashed and uses any defined column,
+		// it has to be enforced on top of ComputeScalar
+		if (CDistributionSpec::EdtHashed ==
+			pdsWorkerRandom->PdsSegmentBase()->Edt())
+		{
+			CDistributionSpecHashed *pdsHashed =
+				CDistributionSpecHashed::PdsConvert(
+					pdsWorkerRandom->PdsSegmentBase());
+			CColRefSet *pcrs = pdsHashed->PcrsUsed(m_mp);
+			BOOL fUsesDefinedCols = FUnaryUsesDefinedColumns(pcrs, exprhdl);
+			pcrs->Release();
+			if (fUsesDefinedCols)
+			{
+				return GPOS_NEW(mp) CDistributionSpecAny(this->Eopid());
+			}
 		}
 	}
 
