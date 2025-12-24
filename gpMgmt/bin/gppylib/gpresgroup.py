c62a74db07e73bdb57a4f3b0acbf2cbd70a7a262
zhangyue <zylcf818@163.com>
2025-12-24 20:58:39 +0800

    Fix gpconfig for resource group v2 on segment hosts
    The gpcheckresgroupv2impl script failed on segments when running
    `gpconfig -c gp_resource_manager -v "group-v2"`.
    
    Root cause:
    The validation script tried to connect to localhost:5432 on each host
    to retrieve gp_resource_group_cgroup_parent. However, segment hosts
    don't run the master database - causing "Connection refused" errors.
    
    Fix:
    - Retrieve gp_resource_group_cgroup_parent from master database in
      gpresgroup.py before dispatching validation commands;
    - Pass the cgroup_parent value to gpcheckresgroupv2impl via command
      line argument (--cgroup-parent);
    - Remove database connection logic from gpcheckresgroupv2impl;

--- gpMgmt/bin/gppylib/gpresgroup.py
+++ gpMgmt/bin/gppylib/gpresgroup.py
@@ -8,6 +8,7 @@ from gppylib.commands.unix import *
 from gppylib.commands.gp import *
 from gppylib.gparray import GpArray
 from gppylib.gplog import get_default_logger
+from gppylib.db import dbconn
 
 
 class GpResGroup(object):
@@ -40,13 +41,40 @@ class GpResGroup(object):
 
     @staticmethod
     def validate_v2():
+        """
+        Validate cgroup v2 configuration on all hosts.
+
+        This method:
+        1. Connects to the master database to retrieve gp_resource_group_cgroup_parent
+        2. Passes this value to gpcheckresgroupv2impl on each host via command line
+        3. Each host validates its local cgroup filesystem permissions
+        """
         pool = base.WorkerPool()
         gp_array = GpArray.initFromCatalog(dbconn.DbURL(), utility=True)
         host_list = list(set(gp_array.get_hostlist(True)))
         msg = None
 
+        # Get cgroup_parent value from master database
+        cgroup_parent = None
+        try:
+            # Connect to master database to get the GUC parameter
+            master_dburl = dbconn.DbURL()
+            with dbconn.connect(master_dburl, utility=True) as conn:
+                sql = "SHOW gp_resource_group_cgroup_parent"
+                cursor = dbconn.query(conn, sql)
+                result = cursor.fetchone()
+                if result and result[0]:
+                    cgroup_parent = result[0]
+                else:
+                    return "failed to retrieve gp_resource_group_cgroup_parent parameter from master database"
+        except Exception as e:
+            return "failed to retrieve gp_resource_group_cgroup_parent parameter: {}".format(str(e))
+
+        # Build command with cgroup_parent parameter
+        cmd_str = "gpcheckresgroupv2impl --cgroup-parent '{}'".format(cgroup_parent)
+
         for h in host_list:
-            cmd = Command(h, "gpcheckresgroupv2impl", REMOTE, h)
+            cmd = Command(h, cmd_str, REMOTE, h)
             pool.addCommand(cmd)
         pool.join()
 
