a8f4f7702913c783b9cad17c5bf4eceeafe664b2
Jianghua Yang <yjhjstz@gmail.com>
2026-03-02 22:41:23 +0800

    open parallel append for repl

--- src/backend/gporca/libgpopt/src/xforms/CXformDynamicGet2ParallelAppendTableScan.cpp
+++ src/backend/gporca/libgpopt/src/xforms/CXformDynamicGet2ParallelAppendTableScan.cpp
@@ -87,8 +87,7 @@ CXformDynamicGet2ParallelAppendTableScan::Exfp(CExpressionHandle &exprhdl) const
 	// Don't use parallel append for replicated tables
 	if (ptabdesc->GetRelDistribution() == IMDRelation::EreldistrReplicated ||
 		ptabdesc->GetRelDistribution() == IMDRelation::EreldistrMasterOnly ||
-		ptabdesc->RetrieveRelStorageType() == IMDRelation::ErelstorageMixedPartitioned ||
-		COptCtxt::PoctxtFromTLS()->HasReplicatedTables())
+		ptabdesc->RetrieveRelStorageType() == IMDRelation::ErelstorageMixedPartitioned)
 	{
 		return CXform::ExfpNone;
 	}
