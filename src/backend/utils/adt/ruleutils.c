ad77f063dfe436e4faee7f2a74b56015e17d0d15
Nathan Bossart <nathan@postgresql.org>
2026-08-10 06:38:24 -0700

    Obstruct EXTRACT() field name deparse injection.
    The parser accepts any string as an EXTRACT() field name, but
    deparsing does not quote and escape it accordingly.  To fix, quote
    and escape the field name during deparsing as needed.  It might be
    a good idea to validate the field name during parsing and
    deparsing, too, but that is left as a future exercise.
    
    Reported-by: Ben Morris in collaboration with Claude and Anthropic Research
    Author: Nathan Bossart <nathandbossart@gmail.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Etsuro Fujita <etsuro.fujita@gmail.com>
    Security: CVE-2026-15741
    Backpatch-through: 14

--- src/backend/utils/adt/ruleutils.c
+++ src/backend/utils/adt/ruleutils.c
@@ -10881,7 +10881,7 @@ get_func_sql_syntax(FuncExpr *expr, deparse_context *context)
 				Assert(IsA(con, Const) &&
 					   con->consttype == TEXTOID &&
 					   !con->constisnull);
-				appendStringInfoString(buf, TextDatumGetCString(con->constvalue));
+				appendStringInfoString(buf, quote_identifier(TextDatumGetCString(con->constvalue)));
 			}
 			appendStringInfoString(buf, " FROM ");
 			get_rule_expr((Node *) lsecond(expr->args), context, false);
@@ -10901,6 +10901,7 @@ get_func_sql_syntax(FuncExpr *expr, deparse_context *context)
 				Assert(IsA(con, Const) &&
 					   con->consttype == TEXTOID &&
 					   !con->constisnull);
+				/* NB: safe because no allowed words need quoted/escaped */
 				appendStringInfo(buf, " %s",
 								 TextDatumGetCString(con->constvalue));
 			}
